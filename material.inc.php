d18552f1e87711da76a015ebdebde0a5077bb729
Ori Avtalion <ori@avtalion.name>
2023-10-07 11:12:44 +0300

    WIP

---
+++ material.inc.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ *------
+ * BGA framework => © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
+ * The Bottle Imp implementation  => © Ori Avtalion <ori@avtalion.name>
+ *
+ * This code has been produced on the BGA studio platform for use on http =>//boardgamearena.com.
+ * See http =>//en.boardgamearena.com/#!doc/Studio for more information.
+ * -----
+ *
+ * material.inc.php
+ *
+ * game material description
+ *
+ * Here, you can describe the material of your game with PHP variables.
+ *
+ * This file is loaded in your game logic class constructor, ie these variables
+ * are available everywhere in your game logic code.
+ *
+ */
+
+$this->cards = [
+	1 => ['rank' => 1, 'suit' => 1, 'points' => 1],
+	2 => ['rank' => 2, 'suit' => 1, 'points' => 1],
+	2.5 => ['rank' => 2.5, 'suit' => 1, 'points' => 1],
+	3 => ['rank' => 3, 'suit' => 1, 'points' => 2],
+	4 => ['rank' => 4, 'suit' => 2, 'points' => 1],
+	4.5 => ['rank' => 4.5, 'suit' => 2, 'points' => 1],
+	5 => ['rank' => 5, 'suit' => 1, 'points' => 2],
+	6 => ['rank' => 6, 'suit' => 2, 'points' => 1],
+	6.5 => ['rank' => 6.5, 'suit' => 2, 'points' => 1],
+	7 => ['rank' => 7, 'suit' => 1, 'points' => 3],
+	8 => ['rank' => 8, 'suit' => 2, 'points' => 2],
+	8.5 => ['rank' => 8.5, 'suit' => 2, 'points' => 2],
+	9 => ['rank' => 9, 'suit' => 1, 'points' => 3],
+	10 => ['rank' => 10, 'suit' => 2, 'points' => 2],
+	10.5 => ['rank' => 10.5, 'suit' => 2, 'points' => 2],
+	11 => ['rank' => 11, 'suit' => 3, 'points' => 1],
+	12 => ['rank' => 12, 'suit' => 1, 'points' => 4],
+	12.5 => ['rank' => 12.5, 'suit' => 1, 'points' => 4],
+	13 => ['rank' => 13, 'suit' => 2, 'points' => 3],
+	14 => ['rank' => 14, 'suit' => 3, 'points' => 1],
+	14.5 => ['rank' => 14.5, 'suit' => 3, 'points' => 1],
+	15 => ['rank' => 15, 'suit' => 1, 'points' => 4],
+	16 => ['rank' => 16, 'suit' => 3, 'points' => 2],
+	16.5 => ['rank' => 16.5, 'suit' => 3, 'points' => 2],
+	17 => ['rank' => 17, 'suit' => 2, 'points' => 3],
+	18 => ['rank' => 18, 'suit' => 1, 'points' => 5],
+	18.5 => ['rank' => 18.5, 'suit' => 1, 'points' => 5],
+	20 => ['rank' => 20, 'suit' => 2, 'points' => 4],
+	20.5 => ['rank' => 20.5, 'suit' => 2, 'points' => 4],
+	21 => ['rank' => 21, 'suit' => 3, 'points' => 2],
+	22 => ['rank' => 22, 'suit' => 1, 'points' => 5],
+	22.5 => ['rank' => 22.5, 'suit' => 1, 'points' => 5],
+	23 => ['rank' => 23, 'suit' => 3, 'points' => 3],
+	24 => ['rank' => 24, 'suit' => 2, 'points' => 4],
+	24.5 => ['rank' => 24.5, 'suit' => 2, 'points' => 4],
+	25 => ['rank' => 25, 'suit' => 1, 'points' => 6],
+	26 => ['rank' => 26, 'suit' => 3, 'points' => 3],
+	26.5 => ['rank' => 26.5, 'suit' => 3, 'points' => 3],
+	27 => ['rank' => 27, 'suit' => 2, 'points' => 5],
+	28 => ['rank' => 28, 'suit' => 1, 'points' => 6],
+	28.5 => ['rank' => 28.5, 'suit' => 1, 'points' => 6],
+	29 => ['rank' => 29, 'suit' => 3, 'points' => 4],
+	30 => ['rank' => 30, 'suit' => 2, 'points' => 5],
+	30.5 => ['rank' => 30.5, 'suit' => 2, 'points' => 5],
+	31 => ['rank' => 31, 'suit' => 3, 'points' => 4],
+	32 => ['rank' => 32, 'suit' => 2, 'points' => 6],
+	32.5 => ['rank' => 32.5, 'suit' => 2, 'points' => 6],
+	33 => ['rank' => 33, 'suit' => 3, 'points' => 5],
+	34 => ['rank' => 34, 'suit' => 2, 'points' => 6],
+	34.5 => ['rank' => 34.5, 'suit' => 2, 'points' => 6],
+	35 => ['rank' => 35, 'suit' => 3, 'points' => 5],
+	36 => ['rank' => 36, 'suit' => 3, 'points' => 6],
+	36.5 => ['rank' => 36.5, 'suit' => 3, 'points' => 6],
+	37 => ['rank' => 37, 'suit' => 3, 'points' => 6],
+];
